f8956058786986bfa472d59ec1585ba8fe599286
Gr0wMan <bletsdone@gmail.com>
2024-05-08 16:20:02 +0300

    bugfix
    "нет такого индекса"
    Боже, я убил полчаса на это

--- src/main/java/com/holymoderation/addon/events/Punishments.java
+++ src/main/java/com/holymoderation/addon/events/Punishments.java
@@ -1,11 +1,14 @@
 package com.holymoderation.addon.events;
 
+import com.labymedia.connect.api.chat.Chat;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.events.client.chat.MessageSendEvent;
 
 import com.holymoderation.addon.ChatUtils.ChatManager;
 import com.holymoderation.addon.ChatUtils.Colors;
 import com.holymoderation.addon.ChatUtils.PunishmentsManager;
+
+import java.util.Arrays;
 
 public class Punishments {
     private static String player = null;
@@ -14,12 +17,25 @@ public class Punishments {
     @Subscribe
     public void OnUpdate(MessageSendEvent event) {
         String message = event.getMessage();
-        String command = message.split(" ")[0];
+        String command = message.split(" ", 0)[0];
 
         if (PunishmentsManager.IsArrayContains(ChatManager.punishmentsCommands, command)) {
             event.setCancelled(true);
+            ChatManager.ClientMessage("Такое наказание есть");
             if (PunishmentsManager.IsArrayContains(ChatManager.tempPunishments, command)) {
+                ChatManager.ClientMessage("Это наказание временное");
                 messageSplit = message.split(" ", 4);
+                switch (messageSplit.length) {
+                    case (1):
+                        ChatManager.ClientMessage(Colors.RED + "Вы не указали ник игрока, время и причину!");
+                        return;
+                    case (2):
+                        ChatManager.ClientMessage(Colors.RED + "Вы не указали время и причину!");
+                        return;
+                    case (3):
+                        ChatManager.ClientMessage(Colors.RED + "Вы не указали причину!");
+                        return;
+                }
                 String nick = messageSplit[1];
                 String time = messageSplit[2];
                 String reason = messageSplit[3];
@@ -30,66 +46,41 @@ public class Punishments {
                     return;
                 }
                 if (PunishmentsManager.IsArrayContains(ChatManager.muteCommands, command)) {
-                    switch (messageSplit.length) {
-                        case (1):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали ник игрока, время и причину мута!");
-                            return;
-                        case (2):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали время и причину мута!");
-                            return;
-                        case (3):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали причину мута!");
-                            return;
-                    }
+                    ChatManager.ClientMessage("Это временный мут");
                     PunishmentsManager.Punish(command, nick, time, reason, false);
                 }
                 if (PunishmentsManager.IsArrayContains(ChatManager.banCommands, command)) {
+                    ChatManager.ClientMessage("Это временный бан");
                     if (!PunishmentsManager.CheckVK()) {
                         return;
-                    }
-                    switch (messageSplit.length) {
-                        case (1):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали ник игрока, время и причину бана!");
-                            return;
-                        case (2):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали время и причину бана!");
-                            return;
-                        case (3):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали причину бана!");
-                            return;
                     }
                     PunishmentsManager.Punish(command, nick, time, reason, true);
                 }
             }
             else if (PunishmentsManager.IsArrayContains(ChatManager.infinityPunishments, command)) {
+                ChatManager.ClientMessage("Это наказание бесконечное");
                 messageSplit = message.split(" ", 3);
+                switch (messageSplit.length) {
+                    case (1):
+                        ChatManager.ClientMessage(Colors.RED + "Вы не указали ник игрока и причину!");
+                        return;
+                    case (2):
+                        ChatManager.ClientMessage(Colors.RED + "Вы не указали причину!");
+                        return;
+                }
                 String nick = messageSplit[1];
                 String reason = messageSplit[2];
                 if (PunishmentsManager.CheckPlayerOnCheck(player, nick)) {
                     return;
                 }
                 if (PunishmentsManager.IsArrayContains(ChatManager.muteCommands, command)) {
-                    switch (messageSplit.length) {
-                        case (1):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали ник игрока и причину мута!");
-                            return;
-                        case (2):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали причину мута!");
-                            return;
-                    }
+                    ChatManager.ClientMessage("Это бесконечный мут");
                     PunishmentsManager.Punish(command, nick, reason, false);
                 }
                 if (PunishmentsManager.IsArrayContains(ChatManager.banCommands, command)) {
+                    ChatManager.ClientMessage("Это бесконечный бан");
                     if (!PunishmentsManager.CheckVK()) {
                         return;
-                    }
-                    switch (messageSplit.length) {
-                        case (1):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали ник игрока и причину бана!");
-                            return;
-                        case (2):
-                            ChatManager.ClientMessage(Colors.RED + "Вы не указали причину бана!");
-                            return;
                     }
                     PunishmentsManager.Punish(command, nick, reason, true);
                 }
